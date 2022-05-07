7f9b56c68b14e2b2684da8409779d6d0adf1e23c
widigdacahya <rizkiwidigda@gmail.com>
2022-05-07 07:15:23 +0700

    array of object

--- json/exercise1.php
+++ json/exercise1.php
@@ -15,5 +15,24 @@ echo $data;
 echo "<br/>";
 var_dump($data);
 
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
+$people =  [
+    [
+        "name" => "Person1",
+        "charactersitic" => "Black",
+        "email" => "doeperson1@gmail.com"
+    ],
+    [
+        "name" => "Person2",
+        "charactersitic" => "White",
+        "email" => "doeperson2@gmail.com"
+    ]
+];
+
+$datapeople = json_encode($people);
+echo $datapeople;
 
 ?>
