80df4ddd5667fba57bfe4b2dea838f45839d5b39
widigdacahya <rizkiwidigda@gmail.com>
2022-05-10 07:56:02 +0700

    data shown appropriete

--- studycase_pizaahut/js/scriptpizzadata.js
+++ studycase_pizaahut/js/scriptpizzadata.js
@@ -1,24 +1,31 @@
-$.getJSON("data/pizza.json", function (data) {
-  //console.log(data);
+function allMenu() {
+  $.getJSON("data/pizza.json", function (data) {
+    //console.log(data);
 
-  let menu = data.menu;
-  //console.log(menu);
+    let menu = data.menu;
+    //console.log(menu);
 
-  //like foreach in php, but it jquery
-  $.each(menu, function (i, datamenu) {
-    $("#daftar-menu").append(
-      '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' +
-        datamenu.gambar +
-        '" class="card-img-top"><div class="card-body"><h5 class="card-title">' +
-        datamenu.nama +
-        '</h5><p class="card-text">' +
-        datamenu.deskripsi +
-        '</p><h4 class="card-title">Rp. ' +
-        datamenu.harga +
-        '</h4><a href="#" class="btn btn-primary"><i class="bi bi-cart-fill"></i>  Pesan</a></div></div></div>'
-    );
+    //html tag here will replaced everthing exsited on the page
+    $("#daftar-menu").html("");
+
+    //like foreach in php, but it jquery
+    $.each(menu, function (i, datamenu) {
+      $("#daftar-menu").append(
+        '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' +
+          datamenu.gambar +
+          '" class="card-img-top"><div class="card-body"><h5 class="card-title">' +
+          datamenu.nama +
+          '</h5><p class="card-text">' +
+          datamenu.deskripsi +
+          '</p><h4 class="card-title">Rp. ' +
+          datamenu.harga +
+          '</h4><a href="#" class="btn btn-primary"><i class="bi bi-cart-fill"></i>  Pesan</a></div></div></div>'
+      );
+    });
   });
-});
+}
+
+allMenu();
 
 $(".nav-link").on("click", function () {
   $(".nav-link").removeClass("active");
@@ -33,6 +40,11 @@ $(".nav-link").on("click", function () {
   //ganti judul
   //karena h1nya di gtml only one, jadi boleh kek gini
   $("h1").html(kategoriFromMenu);
+
+  if (forKategori == "All Menu") {
+    allMenu();
+    return;
+  }
 
   $.getJSON("data/pizza.json", function (dataresult) {
     //tadi diatas pake append, skearng dengan differet apporach
